b16522cc2b23bf1f087befdee3fe70fc87773709
llaoj <qustwwy@163.com>
2024-02-17 21:47:35 +0800

    If you are inactive for 24h, the data will expire.

--- internal/server/clipboards.go
+++ internal/server/clipboards.go
@@ -2,19 +2,24 @@ package server
 
 import (
 	"sync"
+	"time"
 
 	"github.com/llaoj/gcopy/internal/gcopy"
+	"github.com/llaoj/gcopy/pkg/utils"
+	"github.com/sirupsen/logrus"
 )
 
 type Clipboards struct {
 	mu  *sync.RWMutex
 	cbs map[string]*gcopy.Clipboard
+	log *logrus.Logger
 }
 
-func NewClipboards() *Clipboards {
+func NewClipboards(log *logrus.Logger) *Clipboards {
 	return &Clipboards{
 		mu:  &sync.RWMutex{},
 		cbs: make(map[string]*gcopy.Clipboard),
+		log: log,
 	}
 }
 
@@ -28,4 +33,24 @@ func (c *Clipboards) Get(key string) *gcopy.Clipboard {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 	return c.cbs[key]
+}
+
+func (s *Clipboards) Housekeeping() func() {
+	ticker := time.NewTicker(time.Minute)
+
+	go func() {
+		for range ticker.C {
+			for key := range s.cbs {
+				cb := s.Get(key)
+				if time.Since(cb.CreatedAt) > 24*time.Hour {
+					delete(s.cbs, key)
+					s.log.Infof("[%s] Clipboard expired\n", utils.StrMaskMiddle(key))
+				}
+			}
+		}
+	}()
+
+	return func() {
+		ticker.Stop()
+	}
 }
