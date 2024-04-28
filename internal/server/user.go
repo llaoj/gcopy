001c241ad5ebfc4120049ac79b3fe872fff3d32f
老J <hmmmbiubiubiu@gmail.com>
2024-04-28 09:58:56 +0800

    Add client info into verification email.

--- internal/server/user.go
+++ internal/server/user.go
@@ -39,22 +39,24 @@ func (s *Server) emailCodeHandler(c *gin.Context) {
 		subject = fmt.Sprintf("%s是您的验证码", code)
 		clientInfo := ""
 		if ua.OS != "" {
-			clientInfo += " 客户端信息: " + ua.OS
+			clientInfo += "该请求由 " + ua.OS
 			if ua.Name != "" {
-				clientInfo += fmt.Sprintf(" %s.", ua.Name)
+				clientInfo += fmt.Sprintf(" %s", ua.Name)
 			}
+			clientInfo += " 发送.<br>"
 		}
-		body = fmt.Sprintf("请输入您的验证码: %s. 该验证码有效期5分钟.%s 为保护您的账户, 请不要分享这个验证码.", code, clientInfo)
+		body = fmt.Sprintf("请输入您的验证码: %s. 该验证码有效期5分钟.<br>%s为保护您的账户, 请不要分享这个验证码.", code, clientInfo)
 	} else {
 		subject = fmt.Sprintf("%s is your verification code", code)
 		clientInfo := ""
 		if ua.OS != "" {
-			clientInfo += " Client info: " + ua.OS
+			clientInfo += "Requested from " + ua.OS
 			if ua.Name != "" {
-				clientInfo += fmt.Sprintf(" %s.", ua.Name)
+				clientInfo += fmt.Sprintf(" %s", ua.Name)
 			}
+			clientInfo += ".<br>"
 		}
-		body = fmt.Sprintf("Enter the verification code when prompted: %s. Code will expire in 5 minutes.%s To protect your account, do not share this code.", code, clientInfo)
+		body = fmt.Sprintf("Enter the verification code when prompted: %s. Code will expire in 5 minutes.<br>%sTo protect your account, do not share this code.", code, clientInfo)
 	}
 	message := gomail.NewMessage()
 	message.SetHeader("From", message.FormatAddress(s.config.SMTPSender, "GCopy"))
