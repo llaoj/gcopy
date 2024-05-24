3d98b3419c38cb68c364ded93e92ba1e445a20ad
老J <hmmmbiubiubiu@gmail.com>
2024-05-24 08:43:50 +0800

    Add shortcut key support.

--- frontend/components/sync-shortcut.tsx
+++ frontend/components/sync-shortcut.tsx
@@ -29,7 +29,7 @@ export default function SyncShortcut() {
       >
         <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383z" />
       </svg>
-      <span className="mx-2">/</span>
+      <span className="mx-2 opacity-50">/</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 -960 960 960"
