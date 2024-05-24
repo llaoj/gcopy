456a8ae22811f11b817dffb8d91e7a82ae2483dd
老J <hmmmbiubiubiu@gmail.com>
2024-05-24 12:33:49 +0800

    Fix: Error: React Hook "useEffect" is called conditionally.

--- frontend/lib/use-shortcut.ts
+++ frontend/lib/use-shortcut.ts
@@ -2,21 +2,22 @@ import { useEffect } from "react";
 import { isDesktop, isMacOs } from "react-device-detect";
 
 export function useShortcut({ eventHandler }: { eventHandler: () => void }) {
-  if (isDesktop) {
-    useEffect(() => {
-      // refer: https://support.google.com/chrome/answer/157179
-      function keyDownHandler(e: globalThis.KeyboardEvent) {
-        if (
-          e.key === "Enter" ||
-          (isMacOs && e.metaKey && e.key == "1") ||
-          (!isMacOs && e.ctrlKey && e.key == "1")
-        ) {
-          e.preventDefault();
-          eventHandler();
-        }
+  useEffect(() => {
+    function keyDownHandler(e: globalThis.KeyboardEvent) {
+      if (!isDesktop) {
+        return;
       }
-      document.addEventListener("keydown", keyDownHandler);
-      return () => document.removeEventListener("keydown", keyDownHandler);
-    });
-  }
+      // refer: https://support.google.com/chrome/answer/157179
+      if (
+        e.key === "Enter" ||
+        (isMacOs && e.metaKey && e.key == "1") ||
+        (!isMacOs && e.ctrlKey && e.key == "1")
+      ) {
+        e.preventDefault();
+        eventHandler();
+      }
+    }
+    document.addEventListener("keydown", keyDownHandler);
+    return () => document.removeEventListener("keydown", keyDownHandler);
+  });
 }
