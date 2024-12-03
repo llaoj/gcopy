c294caa8b52c5b22262410c440c119e8c92f3e9a
老J <hmmmbiubiubiu@gmail.com>
2024-12-03 16:51:58 +0800

    v1.4.4

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { getTranslations } from "next-intl/server";
 import type { Viewport } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import GoogleAdsense from "@/components/google-adsense";
 
 export async function generateMetadata({
   params: { locale },
@@ -39,6 +40,10 @@ export default function RootLayout({
       {process.env.GOOGLE_ANALYTICS_ID && (
         <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
       )}
+      {process.env.NODE_ENV == "production" &&
+        process.env.GOOGLE_ADSENSE_PID && (
+          <GoogleAdsense pId={process.env.GOOGLE_ADSENSE_PID} />
+        )}
     </html>
   );
 }
---
+++ frontend/components/google-adsense.tsx
@@ -0,0 +1,15 @@
+import Script from "next/script";
+
+export default function GoogleAdsense({ pId }: { pId?: string }) {
+  if (!pId) {
+    return null;
+  }
+
+  return (
+    <Script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-${pId}"
+      crossOrigin="anonymous"
+    />
+  );
+}
